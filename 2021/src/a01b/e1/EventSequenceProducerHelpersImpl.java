470929cbc1eac68fd840a1a6c0e0691b44c4e3a7
Elisa Yan <elisa.yan@studio.unibo.it>
2023-12-06 20:41:07 +0100

    added lib

---
+++ 2021/src/a01b/e1/EventSequenceProducerHelpersImpl.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import a01b.e1.EventSequenceProducerHelpers;
+
+public class EventSequenceProducerHelpersImpl implements EventSequenceProducerHelpers{
+
+    @Override
+    public <E> EventSequenceProducer<E> fromIterator(Iterator<Pair<Double, E>> iterator) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'fromIterator'");
+    }
+
+    @Override
+    public <E> List<E> window(EventSequenceProducer<E> sequence, double fromTime, double toTime) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'window'");
+    }
+
+    @Override
+    public <E> Iterable<E> asEventContentIterable(EventSequenceProducer<E> sequence) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'asEventContentIterable'");
+    }
+
+    @Override
+    public <E> Optional<Pair<Double, E>> nextAt(EventSequenceProducer<E> sequence, double time) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextAt'");
+    }
+
+    @Override
+    public <E> EventSequenceProducer<E> filter(EventSequenceProducer<E> sequence, Predicate<E> predicate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'filter'");
+    }
+    
+}
