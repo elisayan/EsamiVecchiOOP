7e2488e9c33b30a4225bc984a1b8c5592782eaa8
Elisa Yan <elisa.yan@studio.unibo.it>
2023-12-10 18:16:57 +0100

    implementing a01c es1, method fromFile

--- 2021/src/a01c/e1/EventHistoryFactoryImpl.java
+++ 2021/src/a01c/e1/EventHistoryFactoryImpl.java
@@ -1,10 +1,13 @@
 package a01c.e1;
 
 import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
+
+import org.junit.runner.FilterFactory.FilterNotCreatedException;
 
 public class EventHistoryFactoryImpl implements EventHistoryFactory {
 
@@ -30,7 +33,7 @@ public class EventHistoryFactoryImpl implements EventHistoryFactory {
                 if(elements.hasNext()) {
                     currentElement = elements.next();
                     return true;
-                }else {
+                } else {
                     return false;
                 }
             }
@@ -40,26 +43,104 @@ public class EventHistoryFactoryImpl implements EventHistoryFactory {
 
     @Override
     public <E> EventHistory<E> fromIterators(Iterator<Double> times, Iterator<E> content) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'fromIterators'");
+        return new EventHistory<E>() {
+
+            @Override
+            public double getTimeOfEvent() {
+                return times.next();
+            }
+
+            @Override
+            public E getEventContent() {
+                return content.next();
+            }
+
+            @Override
+            public boolean moveToNextEvent() {
+                return times.hasNext() && content.hasNext();
+            }
+            
+        };
     }
 
     @Override
     public <E> EventHistory<E> fromListAndDelta(List<E> content, double initial, double delta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'fromListAndDelta'");
+        return new EventHistory<E>() {
+
+            private int index = 0;
+            private int indexList = 0;
+            @Override
+            public double getTimeOfEvent() {
+                return initial + delta * index++;
+            }
+
+            @Override
+            public E getEventContent() {
+                return content.get(indexList++);
+            }
+
+            @Override
+            public boolean moveToNextEvent() {
+                return indexList < content.size();
+            }
+            
+        };
     }
 
     @Override
     public <E> EventHistory<E> fromRandomTimesAndSupplier(Supplier<E> content, int size) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'fromRandomTimesAndSupplier'");
+        return new EventHistory<E>() {
+
+            private int counter = 0;
+            @Override
+            public double getTimeOfEvent() {
+                return Math.random();
+            }
+
+            @Override
+            public E getEventContent() {
+                return content.get();
+            }
+
+            @Override
+            public boolean moveToNextEvent() {
+                return ++counter < size;
+            }
+            
+        };
     }
 
     @Override
     public EventHistory<String> fromFile(String file) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'fromFile'");
+        return new EventHistory<String>() {
+
+            private BufferedReader content = new BufferedReader(new FileReader(file));
+            private String[] events;
+            private int counter = 0;
+            @Override
+            public double getTimeOfEvent() {
+                try {
+                    events = content.readLine().split(":");
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return Double.parseDouble(events[counter++]);
+            }
+
+            @Override
+            public String getEventContent() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getEventContent'");
+            }
+
+            @Override
+            public boolean moveToNextEvent() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'moveToNextEvent'");
+            }
+            
+        };
     }
 
 }
