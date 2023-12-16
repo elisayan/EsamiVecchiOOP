e58e1ec2ca343bb8217c4560c18f8d71c58d15bd
Elisa Yan <elisa.yan@studio.unibo.it>
2023-12-16 11:10:44 +0100

    debugging

--- 2021/src/a02b/e1/AbstractUniversityProgram.java
+++ 2021/src/a02b/e1/AbstractUniversityProgram.java
@@ -1,15 +1,29 @@
 package a02b.e1;
 
 import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
 
 public abstract class AbstractUniversityProgram implements UniversityProgram {
 
-    Map<String, Pair<Sector, Integer>> courses = new HashMap<>();
+    Map<String, Pair<Sector, Integer>> coursesMap = new HashMap<>();
 
     @Override
     public void addCourse(String name, Sector sector, int credits) {
-        Pair<Sector, Integer> pair = new Pair<Sector, Integer>(sector, credits);
-        courses.put(name, pair);
-
+        coursesMap.put(name, new Pair<Sector, Integer>(sector, credits));
     }
+
+    @Override
+    public boolean isValid(Set<String> courseNames) {
+        Set<Pair<Sector,Integer>> courses = courseNames.stream().map(coursesMap::get).collect(Collectors.toSet());
+        return getConstraint().stream().allMatch(c->isConstraintValid(c, courses));
+    }
+
+    private boolean isConstraintValid(Pair<Predicate<Sector>, Predicate<Integer>> p,
+            Set<Pair<Sector, Integer>> courses) {
+        return p.get2().test(courses.stream().filter(e -> p.get1().test(e.get1()))
+                .mapToInt(c -> c.get2()).sum());
+    }
+
+    protected abstract Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraint();
 }
--- 2021/src/a02b/e1/UniversityProgramFactoryImpl.java
+++ 2021/src/a02b/e1/UniversityProgramFactoryImpl.java
@@ -1,86 +1,68 @@
 package a02b.e1;
 
-import static a02b.e1.UniversityProgram.Sector.COMPUTER_ENGINEERING;
-import static a02b.e1.UniversityProgram.Sector.COMPUTER_SCIENCE;
-import static a02b.e1.UniversityProgram.Sector.MATHEMATICS;
-import static a02b.e1.UniversityProgram.Sector.PHYSICS;
+import static a02b.e1.UniversityProgram.Sector.*;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class UniversityProgramFactoryImpl implements UniversityProgramFactory {
 
     @Override
     public UniversityProgram flexible() {
-        return new UniversityProgram() {
-
-            Map<String, Pair<Sector, Integer>> courses = new HashMap<>();
-            @Override
-            public void addCourse(String name, Sector sector, int credits) {
-                Pair<Sector, Integer> pair = new Pair<Sector,Integer>(sector, credits);
-                courses.put(name, pair);
-            }
+        return new AbstractUniversityProgram() {
 
             @Override
-            public boolean isValid(Set<String> courseNames) {
-                List<Integer> credits = new LinkedList<>();
-                for (String name : courseNames) {
-                    credits.add(courses.get(name).get2());
-                }
-                return credits.stream().mapToInt(Integer::intValue).sum() == 60;
+            protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraint() {
+                return Set.<Pair<Predicate<Sector>, Predicate<Integer>>>of(
+                        new Pair<>(n -> true, c -> c == 60));
             }
-            
         };
     }
 
     @Override
     public UniversityProgram scientific() {
-        return new UniversityProgram() {
-
-            private Map<String, Pair<Sector, Integer>> courses = new HashMap<>();
-            @Override
-            public void addCourse(String name, Sector sector, int credits) {
-                Pair<Sector, Integer> pair = new Pair<Sector,Integer>(sector, credits);
-                courses.put(name, pair);
-            }
+        return new AbstractUniversityProgram() {
 
             @Override
-            public boolean isValid(Set<String> courseNames) {                
-                List<Integer> credits = new LinkedList<>();
-                int sumCredits = 0;
-                int sumMath = 0;
-                int sumComputerScience = 0;
-                int sumPhysics = 0;
-                for (String name : courseNames) {
-                    credits.add(courses.get(name).get2());
-                    sumCredits = credits.stream().mapToInt(Integer::intValue).sum();
-                    credits = new LinkedList<>();
-                    if (courses.get(name).get1().equals(MATHEMATICS)) {
-                        credits.add(courses.get(name).get2());
-                        sumMath = credits.stream().mapToInt(Integer::intValue).sum();
-                    } else if (courses.get(name).get1().equals(COMPUTER_SCIENCE)) {
-                        credits.add(courses.get(name).get2());
-                        sumComputerScience = credits.stream().mapToInt(Integer::intValue).sum();
-                    } else if (courses.get(name).get1().equals(PHYSICS)) {
-                        credits.add(courses.get(name).get2());
-                        sumPhysics = credits.stream().mapToInt(Integer::intValue).sum();
-                    }
-                }
-                return sumCredits == 60 && sumMath >= 12 && sumComputerScience >= 12 && sumPhysics >= 12;
+            protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraint() {
+                return Set.<Pair<Predicate<Sector>, Predicate<Integer>>>of(
+                        new Pair<>(n -> true, c -> c == 60),
+                        new Pair<>(n -> n == MATHEMATICS, c -> c >= 12),
+                        new Pair<>(n -> n == COMPUTER_SCIENCE, c -> c >= 12),
+                        new Pair<>(n -> n == PHYSICS, c -> c >= 12));
             }
-            
         };
+
     }
 
     @Override
     public UniversityProgram shortComputerScience() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shortComputerScience'");
+        return new AbstractUniversityProgram() {
+
+            @Override
+            protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraint() {
+                return Set.<Pair<Predicate<Sector>, Predicate<Integer>>>of(
+                        new Pair<>(n -> true, c -> c >= 48),
+                        new Pair<>(n -> n == COMPUTER_ENGINEERING || n == COMPUTER_SCIENCE, c -> c >= 30));
+            }
+
+        };
     }
 
     @Override
     public UniversityProgram realistic() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'realistic'");
+        return new AbstractUniversityProgram() {
+
+            @Override
+            protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraint() {
+                return Set.<Pair<Predicate<Sector>, Predicate<Integer>>>of(
+                        new Pair<>(n -> true, c -> c == 120),
+                        new Pair<>(n -> n == COMPUTER_ENGINEERING || n == COMPUTER_SCIENCE, c -> c >= 60),
+                        new Pair<>(n -> n == MATHEMATICS || n == PHYSICS, c -> c <= 18),
+                        new Pair<>(n -> n == THESIS, c -> c >= 24));
+            }
+
+        };
     }
 
 }
