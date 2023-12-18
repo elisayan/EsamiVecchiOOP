78ac8521b6fb1223058e624de78175bd9f3ac462
Elisa Yan <elisa.yan@studio.unibo.it>
2023-12-18 17:51:13 +0100

    reset a01b e1

--- 2021/src/a01b/e1/EventSequenceProducerHelpersImpl.java
+++ 2021/src/a01b/e1/EventSequenceProducerHelpersImpl.java
@@ -2,90 +2,39 @@ package a01b.e1;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Predicate;
 
-public class EventSequenceProducerHelpersImpl implements EventSequenceProducerHelpers{
-
-    public EventSequenceProducerHelpersImpl() {
-        
-    }
+public class EventSequenceProducerHelpersImpl implements EventSequenceProducerHelpers {
 
     @Override
     public <E> EventSequenceProducer<E> fromIterator(Iterator<Pair<Double, E>> iterator) {
-        return () -> iterator.next();
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'fromIterator'");
     }
 
     @Override
     public <E> List<E> window(EventSequenceProducer<E> sequence, double fromTime, double toTime) {
-        List<E> list = new LinkedList<>();
-        Pair<Double, E> elem = sequence.getNext();
-        while (elem.get1() < toTime) {
-            if (elem.get1() >= fromTime) {
-                list.add(elem.get2());
-            }
-            elem = sequence.getNext();            
-        }                
-        return list;
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'window'");
     }
 
     @Override
     public <E> Iterable<E> asEventContentIterable(EventSequenceProducer<E> sequence) {
-        return () -> new Iterator<E>() {
-            Pair<Double, E> elem;
-
-            @Override
-            public boolean hasNext() {
-                try {
-                    elem = sequence.getNext();
-                } catch (NoSuchElementException e) {
-                    return false;
-                }
-                return elem != null;
-            }
-
-            @Override
-            public E next() {
-                return elem.get2();
-            }
-        };
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'asEventContentIterable'");
     }
 
     @Override
     public <E> Optional<Pair<Double, E>> nextAt(EventSequenceProducer<E> sequence, double time) {
-        Pair<Double, E> event = sequence.getNext();
-        while (event != null) {
-            if (event.get1() > time) {
-                return Optional.ofNullable(event);
-            }
-            try {
-                event = sequence.getNext();
-            } catch (Exception e) {
-                return Optional.empty();
-            }
-
-        }
-        return Optional.empty();
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextAt'");
     }
 
     @Override
     public <E> EventSequenceProducer<E> filter(EventSequenceProducer<E> sequence, Predicate<E> predicate) {
-        return new EventSequenceProducer<E>() {
-
-            @Override
-            public Pair<Double, E> getNext() throws NoSuchElementException {
-                Pair<Double, E> event = sequence.getNext();
-                while (event != null) {
-                    if (predicate.test(event.get2())) {
-                        return event;
-                    }
-                    event = sequence.getNext();
-                }
-                return event;
-            }
-
-        };
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'filter'");
     }
+
 }
