ef51af1f7334b04d83030a5ad7f2a7b319533ba2
Elisa Yan <elisa.yan@studio.unibo.it>
2024-01-12 21:24:59 +0100

    implementing a01a e2

--- 2023/src/a01a/e2/GUI.java
+++ 2023/src/a01a/e2/GUI.java
@@ -2,32 +2,48 @@ package a01a.e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
+    private final Map<JButton, Pair<Integer,Integer>> cells = new HashMap<>();
+    private final Logics logic;
+    private int c=1;
     
     public GUI(int size) {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(100*size, 100*size);
+        this.logic=new LogicsImpl(size);
         
         JPanel panel = new JPanel(new GridLayout(size,size));
         this.getContentPane().add(panel);
         
         ActionListener al = e -> {
             var jb = (JButton)e.getSource();
-        	jb.setText(String.valueOf(cells.indexOf(jb)));
+            var position = cells.get(jb);
+            var result = logic.hit(position.getX(), position.getY());
+
+            if (result) {
+                for (var entry : cells.entrySet()) {
+                    if (logic.isHitted(entry.getValue().getX(), entry.getValue().getY())) {
+                        entry.getKey().setText(String.valueOf(c++));
+                    }
+                }
+            }
+            
+            if (this.logic.isOver()) {
+                System.exit(0);
+            }
         };
+
+        
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
-            	var pos = new Pair<>(j,i);
-                final JButton jb = new JButton(pos.toString());
-                this.cells.add(jb);
+                final JButton jb = new JButton();
+                this.cells.put(jb, new Pair<>(i,j));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
---
+++ 2023/src/a01a/e2/Logics.java
@@ -0,0 +1,7 @@
+package a01a.e2;
+
+public interface Logics {
+    boolean hit(int x, int y);
+    boolean isHitted(int x, int y);
+    boolean isOver();    
+}
